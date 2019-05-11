8755a9c86dbdc130d97919659b578c78d0adcade
Lawrence Lockhart <ldlockhart@gmail.com>
2019-05-11 06:40:45 -0500

    Add hamburger menu, additional sass varialbes

---
+++ dist/js/main.js
@@ -0,0 +1,6 @@
+
+const menu-Btn = document.querySelector('.btn-menu');
+const menu = document.querySelector('.menu');
+const menu-Branding = document.querySelector('.menu-branding');
+const menu-nav = document.querySelector('menu-nav');
+const menu-Item = document.querySelectorAll('menu-item');
