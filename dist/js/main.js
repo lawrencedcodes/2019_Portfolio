ab277e59e05392a83210e33ba21e14a12904d4d1
Lawrence Lockhart <ldlockhart@gmail.com>
2019-05-11 07:38:15 -0500

    Add menu button,js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,29 @@
 
-const menu-Btn = document.querySelector('.btn-menu');
+//Set Variables
+const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const menu-Branding = document.querySelector('.menu-branding');
-const menu-nav = document.querySelector('menu-nav');
-const menu-Item = document.querySelectorAll('menu-item');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.nav-item');
+let showMenu = false;
+
+
+menuBtn.addEventListener('click',toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu) {
+        menuBtn.classList.add('close')
+        menu.classList.add('show')
+        menuBranding.classList.add('show')
+        menuNav.classList.add('show')
+        navItem.forEach(item => item.classList.add('show'));
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close')
+        menu.classList.remove('show')
+        menuBranding.classList.remove('show')
+        menuNav.classList.remove('show')
+        navItem.forEach(item => item.classList.remove('show'));
+        showMenu = false;
+    }
+}
